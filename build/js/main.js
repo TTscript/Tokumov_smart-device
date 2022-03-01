f7304d318a678b69420fe90f8af6b89b26f7ab7a
Timur <t.tokumov@gmail.com>
2022-03-01 23:03:54 +0600

    finished the project

--- build/js/main.js
+++ build/js/main.js
@@ -1,120 +1,257 @@
-/*jshint esversion: 6 */
+// /*jshint esversion: 6 */
 
-const pageHeader = document.querySelector('.page-header');
-const burgerClosed = document.querySelector('.burger-closed');
-const burgerOpen = document.querySelector('.burger-open');
-const burgerTop = document.querySelector('.burger-closed__top-line');
-const burgerBottom = document.querySelector('.burger-closed__bottom-line');
-const burgerMiddle = document.querySelector('.burger-closed__middle-line');
-const navigationItem = document.querySelectorAll('.page-header__navigation-item');
-const navItemArray = Array.from(navigationItem);
-const burgerMenuItems = Array.from(document.querySelectorAll('.burger-open__list-item-wrapper'));
-const logoTablet = document.querySelector('.page-header__tablet');
-const mainNavigation = document.querySelector('.main-navigation');
-const pageMain = document.querySelector('.page-main');
-const aboutUsBlock = document.querySelector('.about-us__logo');
-const centerContainer = document.querySelector('.center-container');
+// const sitePartitionsTitle = document.querySelector('.partitions')
+//   .querySelector('h3');
+// const sitePartitionsList = document.querySelector('.partitions__list');
+// const siteAddressTitle = document.querySelector('.address')
+//   .querySelector('h3');
+// const siteAddressContacts = document.querySelector('.address__inner');
+// const pageHeaderButton = document.querySelector('.page-header__contacts').querySelector('button');
+// const bodyTag = document.querySelector('.page-body');
+// const popup = document.querySelector('#popup').content.querySelector('.popup');
+// const popupCloseButton = popup.querySelector('.popup__close-button');
+// const popupShapeInputName = popup.querySelector('.popup__shape-inner-enter-name').querySelector('input');
+// const shape = document.querySelector('.shape');
+// const shapeName = document.querySelector('#name-input');
+// const shapePhone = document.querySelector('#tel-input');
+// const shapeTextarea = document.querySelector('#text-input');
+// const popupShape = popup.querySelector('.popup__shape');
+// const popupShapeName = popup.querySelector('#name-popup-input');
+// const popupShapePhone = popup.querySelector('#tel-popup-input');
 
-mainNavigation.classList.remove('main-navigation--fixed');
+// //ACORDION
 
-window.addEventListener('DOMContentLoaded', correctHeaderHeight);
-let pageHeaderHeight = pageHeader.getBoundingClientRect().height;
-pageMain.style.marginTop = `-${pageHeaderHeight}px`;
+// let sitePartitionsOpen = false;
+// let addressOpen = false;
 
-window.addEventListener('resize', () => {
-  pageHeaderHeight = pageHeader.getBoundingClientRect().height;
-  pageMain.style.marginTop = `-${pageHeaderHeight}px`;
-  pageHeader.addEventListener('change', correctHeaderHeight);
-});
+// sitePartitionsList.classList.remove('partitions__list--appear');
+// sitePartitionsTitle.classList.add('partitions--plus-appear');
+// siteAddressContacts.classList.remove('address__inner--appear');
+// siteAddressTitle.classList.add('address--plus-appear');
+
+// sitePartitionsTitle.addEventListener('click', () => {
+//   sitePartitionsOpen = !sitePartitionsOpen;
+//   sitePartitionsList.classList.toggle('partitions__list--appear');
+//   sitePartitionsTitle.classList.toggle('partitions--plus-appear');
+
+//   if (addressOpen === true) {
+//     siteAddressContacts.classList.remove('address__inner--appear');
+//     siteAddressTitle.classList.add('address--plus-appear');
+//     addressOpen = !addressOpen;
+//   }
+// });
+
+// siteAddressTitle.addEventListener('click', () => {
+//   addressOpen = !addressOpen;
+//   siteAddressContacts.classList.toggle('address__inner--appear');
+//   siteAddressTitle.classList.toggle('address--plus-appear');
+
+//   if (sitePartitionsOpen === true) {
+//     sitePartitionsList.classList.remove('partitions__list--appear');
+//     sitePartitionsTitle.classList.add('partitions--plus-appear');
+//     sitePartitionsOpen = !sitePartitionsOpen;
+//   }
+// });
+
+// //POPUP
+// const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+
+// const createPopup = () => {
+//   const errorMessageTemplate = popup.cloneNode(true);
+//   bodyTag.appendChild(popup);
+//   return errorMessageTemplate;
+// };
+
+// const closePopupClick = () => {
+//   popup.remove();
+//   popupCloseButton.removeEventListener('click', closePopupClick);
+// };
+
+// const closePopupEsc = (evt) => {
+//   if (isEscEvent(evt)) {
+//     popup.remove();
+//   }
+//   window.removeEventListener('keydown', closePopupEsc);
+// };
+
+// const closePopupOverlay = (evt) => {
+//   if (evt.target === popup) {
+//     popup.remove();
+//     window.removeEventListener('click', closePopupOverlay);
+//   }
+// };
+
+// const closePopup = () => {
+//   popupCloseButton.addEventListener('click', closePopupClick);
+//   window.addEventListener('keydown', closePopupEsc);
+//   window.addEventListener('click', closePopupOverlay);
+// };
+
+// function install() {
+//   createPopup();
+//   setTimeout(() => {
+//     closePopup();
+//   }, 50);
+//   popupShapeInputName.focus();
+// }
+
+// // install();
+
+// pageHeaderButton.addEventListener('click', install);
+
+// //LOCAL STORAGE
+// shape.addEventListener('submit', () => {
+//   // evt.preventDefault();
+//   localStorage.setItem('name', `${shapeName.value}`);
+//   localStorage.setItem('tel', `${shapePhone.value}`);
+//   localStorage.setItem('question', `${shapeTextarea.value}`);
+//   // shape.reset();
+// });
+
+// //FORM VALIDATION
+// const nameWrapper = document.querySelector('.shape__inner-enter-name');
+// const phoneWrapper = document.querySelector('.shape__inner-enter-phone');
+
+// function validateName(name) {
+//   const re = /^[a-z\d. !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~А-яЁё]{3,}$/i;
+//   return re.test(String(name));
+// }
+
+// shape.onsubmit = function() {
+//   const nameVal = shapeName.value,
+//     telVal = shapePhone.value;
+
+//   if (nameVal === '') {
+//     nameWrapper.classList.add('shape__error');
+//     return false;
+//   } else {
+//     nameWrapper.classList.remove('shape__error');
+//   }
+
+//   if (!validateName(nameVal)) {
+//     nameWrapper.classList.add('shape__error');
+//     return false;
+//   } else {
+//     nameWrapper.classList.remove('shape__error');
+//   }
+
+//   if (telVal === '' || telVal.length < 17) {
+//     phoneWrapper.classList.add('shape__error');
+//     return false;
+//   } else {
+//     phoneWrapper.classList.remove('shape__error');
+//   }
+// };
+
+// //POPUP VALIDATION
+// const popupNameWrapper = popup.querySelector('.popup__shape-inner-enter-name');
+// const popupTelWrapper = popup.querySelector('.popup__shape-inner-enter-phone');
+
+// function validatePopupName(name) {
+//   const re = /^[a-z\d. !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~А-яЁё]{3,}$/i;
+//   return re.test(String(name));
+// }
+
+// popupShape.onsubmit = function() {
+//   const popupNameVal = popupShapeName.value,
+//     popupTelVal = popupShapePhone.value;
+
+//   if (popupNameVal === '') {
+//     popupNameWrapper.classList.add('popup__shape-error');
+//     return false;
+//   } else {
+//     popupNameWrapper.classList.remove('popup__shape-error');
+//   }
+
+//   if (!validatePopupName(popupNameVal)) {
+//     popupNameWrapper.classList.add('popup__shape-error');
+//     return false;
+//   } else {
+//     popupNameWrapper.classList.remove('popup__shape-error');
+//   }
+
+//   if (popupTelVal === '' || popupTelVal.length < 17) {
+//     popupTelWrapper.classList.add('popup__shape-error');
+//     return false;
+//   } else {
+//     popupTelWrapper.classList.remove('popup__shape-error');
+//   }
+// };
+
+// //MASK
+// const mask = (selector) => {
+
+//   const setCursorPosition = (pos, elem) => {
+//     elem.focus();
+
+//     if (elem.setSelectionRange) {
+//       elem.setSelectionRange(pos, pos);
+//     } else if (elem.createTextRange) {
+//       const range = elem.createTextRange();
+
+//       range.collapse(true);
+//       range.moveEnd('character', pos);
+//       range.moveStart('character', pos);
+//       range.select();
+//     }
+//   };
+
+//   function createMask(event) {
+//     const matrix = '+7(___) ___ __ __',
+//       def = matrix.replace(/\D/g, '');
+
+//     let i = 0,
+//       val = this.value.replace(/\D/g, '');
+//     if (def.length >= val.length) {
+//       val = def;
+//     }
+
+//     // eslint-disable-next-line no-nested-ternary
+//     this.value = matrix.replace(/./g, (a) => /[_\d]/.test(a) && i < val.  length ? val.charAt(i++) : i >= val.length ? '' : a);
+
+//     if (event.type === 'blur') {
+//       if (this.value.length === 2) {
+//         this.value = '';
+//       }
+//     } else {
+//       setCursorPosition(this.value.length, this);
+//     }
+//   }
+
+//   selector.addEventListener('input', createMask);
+//   selector.addEventListener('focus', createMask);
+//   selector.addEventListener('blur', createMask);
+// };
+
+// mask(shapePhone);
+// mask(popupShapePhone);
 
 
-function correctHeaderHeight() {
-  pageHeaderHeight = pageHeader.getBoundingClientRect().height;
-  pageMain.style.marginTop = `-${pageHeaderHeight}px`;
-}
+// const pageHeader = document.querySelector('.page-header');
+// const pageMain = document.querySelector('.page-main');
+// let svgHeight = 12;
 
-// BURGER MENU
+// const maxTablet = window.matchMedia('(max-width: 1023px) and (min-width: 768px)');
+// const maxMobile = window.matchMedia('(max-width: 767px)');
 
-aboutUsBlock.classList.add('about-us__logo--disappear');
-mainNavigation.classList.add('main-navigation--disappear');
-logoTablet.classList.add('page-header__tablet-disappear');
-burgerClosed.classList.add('burger-closed--change-display');
-burgerOpen.classList.add('burger-open--appear');
-pageHeader.classList.remove('page-header--background-color');
-navItemArray.forEach((item) => {
-  item.classList.add('page-header__navigation-item--disappear');
-});
+// window.addEventListener('DOMContentLoaded', correctHeaderHeight);
+// if (maxTablet.matches) {
+//   svgHeight = 23;
+// } else if (maxMobile.matches) {
+//   svgHeight = 39;
+// }
 
-burgerOpen.classList.add('burger-open__action');
+// let pageHeaderHeight = pageHeader.getBoundingClientRect().height;
+// pageMain.style.marginTop = `-${pageHeaderHeight + svgHeight}px`;
 
-burgerClosed.addEventListener('click', () => {
-  burgerOpen.classList.toggle('burger-open__action');
-  burgerTop.classList.toggle('burger-closed__top-line--transform');
-  burgerBottom.classList.toggle('burger-closed__bottom-line--transform');
-  burgerMiddle.classList.toggle('visually-hidden');
-  burgerClosed.classList.toggle('burger-closed--background-color');
-  centerContainer.classList.toggle('center-container--fixed');
+// window.addEventListener('resize', () => {
+//   pageHeaderHeight = pageHeader.getBoundingClientRect().height;
+//   pageMain.style.marginTop = `-${pageHeaderHeight + svgHeight}px`;
+//   pageHeader.addEventListener('change', correctHeaderHeight);
+// });
 
-  burgerMenuItems.forEach((item) => {
-    item.addEventListener('click', () => {
-      centerContainer.classList.remove('center-container--fixed');
-      burgerOpen.classList.add('burger-open__action');
-      burgerTop.classList.remove('burger-closed__top-line--transform');
-      burgerBottom.classList.remove('burger-closed__bottom-line--transform');
-      burgerMiddle.classList.remove('visually-hidden');
-      burgerClosed.classList.remove('burger-closed--background-color');
-    });
-  });
-});
+// function correctHeaderHeight() {
+//   pageHeaderHeight = pageHeader.getBoundingClientRect().height;
+//   pageMain.style.marginTop = `-${pageHeaderHeight + svgHeight}px`;
+// }
 
-
-//FORM VALIDATION
-
-const shape = document.querySelector('.shape');
-const userName = document.querySelector('#user-name');
-const nameWrapper = document.querySelector('.shape__enter-name');
-const tel = document.querySelector('#user-phone');
-const telWrapper = document.querySelector('.shape__enter-phone');
-
-function validateName(name) {
-  const re = /^[a-z\d. !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~А-яЁё]{3,}$/i;
-  return re.test(String(name));
-}
-
-function validatePhone(phone) {
-  const re = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
-  return re.test(String(phone));
-}
-
-shape.onsubmit = function() {
-  const nameVal = userName.value,
-    phoneVal = tel.value;
-
-  if (nameVal === '') {
-    nameWrapper.classList.add('shape__error');
-    return false;
-  } else {
-    nameWrapper.classList.remove('shape__error');
-  }
-
-  if (!validateName(nameVal)) {
-    nameWrapper.classList.add('shape__error');
-    return false;
-  } else {
-    nameWrapper.classList.remove('shape__error');
-  }
-
-  if (phoneVal === '') {
-    telWrapper.classList.add('shape__error');
-    return false;
-  } else {
-    telWrapper.classList.remove('shape__error');
-  }
-
-  if (!validatePhone(phoneVal)) {
-    telWrapper.classList.add('shape__error');
-    return false;
-  } else {
-    telWrapper.classList.remove('shape__error');
-  }
-};
--- source/README.md
+++ source/README.md
@@ -5,3 +5,5 @@
 
 Проверить проект на наличие ошибок - npm test.
 Для корректной работы тестирования необходимо зайти в корень проекта затем зайти в папку node_modules -> stylelint-config-htmlacademy -> node_modules -> stylelint-config-recommended -> index.js -> удалить 19 строку (название строки: 'function-calc-no-invalid': true,) и сохранить изменения.
+
+Кроме того, необходимо проделать похожую процедуру для другой папки - node_modules -> stylelint-config-htmlacademy -> index.js -> удалить 45 строку (название строки: 'property-no-vendor-prefix': true,) и сохранить изменения.
